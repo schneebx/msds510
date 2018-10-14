edbd59684f35ac2c4ef653dbdf4a406a8f2e5148
Alan Hsu <alan.hsu89@gmail.com>
2018-10-14 14:08:50 -0600

    completed week 6 assignment

---
+++ src/msds510/utils/date_functions.py
@@ -0,0 +1,22 @@
+import datetime
+
+
+def get_month(input_date):
+    months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
+    for x in range(0, 12):
+        if months[x] in input_date.lower():
+            return x + 1
+    return None
+
+
+def get_date_joined(input_year, input_day):
+    output_month = get_month(input_day)
+    output_year = int(input_year)
+    return datetime.date(output_year, output_month, 1)
+
+
+def days_since_joined(year, day):
+    today = datetime.date.today()
+    temp_date = get_date_joined(year, day)
+    output_int = (today - temp_date).days
+    return output_int
--- src/read_csv_dict.py
+++ src/read_csv_dict.py
@@ -1,28 +1,6 @@
-# Option 1
 import sys
 import csv
 
-input_file = sys.argv[1]
-
-
-def reading(input):
-    with open(input, 'r') as f:
-        temp_file = csv.DictReader(f)
-        keys = temp_file.fieldnames
-        output_file = []
-        for row in temp_file:
-            output_file.append(row)
-        for key in keys:
-            print(key + ':', output_file[160][key], end=' ')
-
-
-if __name__ == '__main__':
-    reading(input_file)
-
-
-# Option 2
-import sys
-import csv
 
 input_file = sys.argv[1]
 
