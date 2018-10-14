0bf9a2b3ba1c1437089a30b2e65e8e3ed85327b7
Alan Hsu <alan.hsu89@gmail.com>
2018-10-14 15:01:27 -0600

    added some comments

--- src/msds510/utils/date_functions.py
+++ src/msds510/utils/date_functions.py
@@ -4,6 +4,7 @@ import datetime
 def get_month(input_date):
     months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec']
     for x in range(0, 12):
+        # "if in" statement in strings checks for the substring so the input date order doesn't matter as long as it's a string.
         if months[x] in input_date.lower():
             return x + 1
     return None
@@ -18,5 +19,6 @@ def get_date_joined(input_year, input_day):
 def days_since_joined(year, day):
     today = datetime.date.today()
     temp_date = get_date_joined(year, day)
+    # Convert the date difference into days
     output_int = (today - temp_date).days
     return output_int
